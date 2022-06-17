12728828c90e6ec771b305461c330d7f681dfba8
kevincoke <107365195+kevincoke@users.noreply.github.com>
2022-06-17 12:03:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ function updateClock() {
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
-  let ampm = 'AM';
+  let am = 'AM';
+  let pm = 'PM';
+  /*let ampm = 'AM';
 
   if (h > 12) {
     h = h - 12;
     ampm = 'PM';
   }
+  */
 
   h = h < 10 ? '0' + h : h;
   m = m < 10 ? '0' + m : m;
